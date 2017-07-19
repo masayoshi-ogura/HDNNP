299daddf057ae6844218ed3260bb608403fcaf90
jas <hogehoge.example.com>
2017-07-19 19:36:54 +0900

    my_func大幅改修。 方針：力の計算時、原子数nと方向数3を別軸にして配列に入れていたが、各コードで3*nの１つの軸にして計算するように修正する。 symmetric_functionの計算でRc,Rs,etaをリストで受け取り、複数のG,dGを生成して返すようにした。 また、symmetric_functionの計算で間違いを見つけたので修正した(i=jの時に0以上の数となり、それを足し上げてしまっていた)

--- my_func.py
+++ my_func.py
@@ -2,10 +2,16 @@
 
 from mpi4py import MPI
 import numpy as np
+#from quippy import *
 
 import hdnnp
 
 # 原子間ベクトルとその長さ
+### input
+# extend: Atoms Object
+### output
+# r: numpy array (natom x 8*natom x 3)
+# R: numpy array (natom x 8*ntaom)
 def vector_ij(extend, natom):
     r = np.zeros((natom, len(extend), 3))
     R = np.zeros((natom, len(extend)))
@@ -15,55 +21,89 @@ def vector_ij(extend, natom):
     return r, R
 
 # 対称関数と全方向に関する微分
-def symmetric_func(atoms_obj, extend, natom, Rc, Rs, eta):
-    r, R = vector_ij(extend, natom)
-    filter = R > Rc
-    tanh = np.tanh(1 - R / Rc)
-    f = np.exp(-eta * (R - Rs) ** 2)
-    
-    # 一番簡単な対称関数
-    #gij = tanh ** 3
-    ###############
-    
-    # 次に難しいやつ
-    gij = f * (tanh **3)
-    ###########
-    
-    gij[filter] = 0
-    G = np.dot(gij, np.ones(len(extend)))
-    
-    # 微分を求める
-    dG = []
-    for i in range(natom):
-        dGi = np.zeros((natom, 3))
-        for j in range(len(extend)):
-            # RijがRc以上、またはiとjが同原子なら、ゼロ行列のまま
-            if R[i][j] > Rc or i == j:
-                pass
-            else:
-                # dRij: ∂Rij / ∂rkα
-                # n×3行列
-                # iとjが変わるたびに計算し直す必要がある？
-                dRij = np.zeros((natom, 3))
-                for k in range(natom):
-                    if k == i:
-                        dRij[k] = - r[i][j] / R[i][j]
-                    elif k == j:
-                        dRij[k] = r[i][j] / R[i][j]
-                        
-                # 対称関数は一番簡単やなつ
-                #dgij =  (-3 / Rc) * (tanh[i][j] ** 2) * (1 - tanh[i][j] ** 2) * dRij
-                ###################
+### input
+# atoms_objs: list of Atoms Object
+# Rcs,Rss,etas: list of float
+### output
+# Gs: numpy array (nsample x natom x gnum)
+# dGs: numpy array (nsample x natom x 3*natom * gnum)
+def symmetric_func(atoms_objs, natom, nsample, Rcs, Rss, etas):
+    # Rc, Rs, etaを配列で受け取り、その相乗数gnumのG,dGを配列で返す
+    gnum = len(Rcs)*len(Rss)*len(etas)
+    Gs = np.empty((nsample, natom, gnum)) # nsample x natom x gnum 個の配列
+    dGs = np.emtpy((nsample, natom, 3*natom, gnum)) # nsample x natom x 3*natom x gnum 個の配列
+    for m in range(nsample):
+        extend = atoms_objs[m].repeat(2)
+        r, R = vector_ij(extend, natom)
+        # 後で転置してGs,dGsに加える
+        G = np.empty((gnum, natom)) # gnum x natom
+        dG = np.empty((gnum, 3*natom, natom)) # gnum x 3*natom x natom
+        k = 0
+        for Rc in Rcs:
+            for Rs in Rss:
+                for eta in etas:
+                    filter1 = R > Rc
+                    filter2 = R == 0.0
+                    filter = np.logical_or(filter1,filter2)
+                    tanh = np.tanh(1 - R / Rc)
+                    f = np.exp(-eta * (R - Rs) ** 2)
+                    
+                    # 一番簡単な対称関数
+                    #gij = tanh ** 3
+                    ###############
+                    
+                    # 次に難しいやつ
+                    gij = f * (tanh **3)
+                    ###########
+                    
+                    gij[filter] = 0
+                    G[k] = np.dot(gij, np.ones(len(extend)))
+                    
+                    # 微分を求める
+                    dG_k = np.zeros((natom,3*natom))
+                    for i in range(natom):
+                        for j in range(len(extend)):
+                            # RijがRc以上、またはiとjが同原子なら、ゼロ行列のまま
+                            if R[i][j] > Rc or i == j:
+                                pass
+                            else:
+                                # dRij: ∂Rij / ∂rkα
+                                # n×3行列
+                                # iとjが変わるたびに計算し直す必要がある？
+                                dRij = np.zeros((natom, 3))
+                                for l in range(natom):
+                                    if l == i:
+                                        dRij[l] = - r[i][j] / R[i][j]
+                                    elif l == j:
+                                        dRij[l] = r[i][j] / R[i][j]
+                                dRij = dRij.reshape(3*natom)
+                                        
+                                # 対称関数は一番簡単やなつ
+                                #dgij =  (-3 / Rc) * (tanh[i][j] ** 2) * (1 - tanh[i][j] ** 2) * dRij
+                                ###################
 
-                # 次に難しいやつ
-                dgij = - f[i][j] * (tanh[i][j] ** 2) * ((2 * tanh[i][j] * eta * (R[i][j] - Rs)) + (3 * (1 - (tanh[i][j] ** 2)) / Rc)) * dRij
-                ###########
-                dGi += dgij
-        dG.append(dGi)
-    
-    return G, dG
+                                # 次に難しいやつ
+                                dgij = - f[i][j] * (tanh[i][j] ** 2) * ((2 * tanh[i][j] * eta * (R[i][j] - Rs)) + (3 * (1 - (tanh[i][j] ** 2)) / Rc)) * dRij
+                                ###########
+                                dG_k[i] += dgij
+                    dG[k] = dG_k.T
+                    k+=1
+        Gs[m] = G.T
+        dGs[m] = dG.T
+    return Gs, dGs
 
 # RMSEを計算する
+### input
+# comm, rank: MPI communicator, rank of the processor
+# nnp: hdnnp.single_nnp instance
+# dataset: list of following 4 objects
+#          energy: float
+#          forces: numpy array (3*natom)
+#          G: numpy array (natom x gnum)
+#          dG: numpy array (natom x 3*natom x gnum)
+### output
+# E_RMSE: float
+# F_RMSE: float
 def calc_RMSE(comm, rank, nnp, natom, nsample, dataset):
     E_RMSE = 0.0
     F_RMSE = 0.0
@@ -75,15 +115,17 @@ def calc_RMSE(comm, rank, nnp, natom, nsample, dataset):
         E_out = hdnnp.query_E(comm, nnp, G[rank], natom)
         F_rout = hdnnp.query_F(comm, nnp, G[rank], dG[rank], natom)
         E_RMSE += (Et - E_out[0]) ** 2
-        for k in range(natom):
-            for l in range(3):
-                  F_RMSE += (Frt[k][l] - F_rout[k][l]) ** 2
+        for r in range(3*natom):
+            F_RMSE += (Frt[r] - F_rout[r]) ** 2
     E_RMSE /= nsample
     F_RMSE /= (nsample * natom * 3)
     
     return E_RMSE, F_RMSE
 
 # デバッグ用にリストをファイルに書き出す関数を定義
+### input
+# file: file object
+# list: list object
 def output_list(file, list):
     for line in list:
         file.writelines(map(str, line))
