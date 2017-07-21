8f88220ca390bb5af8a3de5eefe5cc7c6143a21a
Masayoshi Ogura <jas@MasayoshinoMacBook-Pro.local>
2017-07-21 16:19:00 +0900

    修正

--- calc_symmetric_func.py
+++ calc_symmetric_func.py
@@ -6,9 +6,10 @@ import my_func
 
 name = 'Ge'
 
-train_dir = 'training_data/xyz/'
-alldataset = AtomsReader(train_dir+'AllSiGe.xyz')
-rawdataset = [data for data in alldataset if data.config_type == 'CrystalSi0Ge8' and data.cohesive_energy < 0.0]
+xyz_dir = 'training_data/xyz/'
+npy_dir = 'training_data/npy/'
+alldataset = AtomsReader(xyz_dir+'AllSiGe.xyz')
+rawdataset = [data for data in alldataset if data.config_type == 'CrystalSi0Ge8' and data.cohesive_energy < -10.0]
 cordinates = [data for data in rawdataset]
 nsample = len(rawdataset)
 natom = 8
@@ -20,7 +21,7 @@ Rss = [1.0]
 etas = [0.0]
 gnum = len(Rcs)*len(Rss)*len(etas)
 Gs,dGs = my_func.symmetric_func(cordinates, natom, nsample, gnum, Rcs, Rss, etas)
-np.save(train_dir+name+'-Es.npy', Es)
-np.save(train_dir+name+'-Fs.npy', Fs)
-np.save(train_dir+name+'-Gs.npy', Gs)
-np.save(train_dir+name+'-dGs.npy', dGs)
+np.save(npy_dir+name+'-Es.npy', Es)
+np.save(npy_dir+name+'-Fs.npy', Fs)
+np.save(npy_dir+name+'-Gs.npy', Gs)
+np.save(npy_dir+name+'-dGs.npy', dGs)
