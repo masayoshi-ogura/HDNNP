342ce79f991dab9b724831e601f379a071cdb66a
ogura <ogura@10.8.88.126>
2017-07-21 12:24:48 +0900

    forte用のjobとiris用のjobを両方作成

---
+++ iris.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+#$ -cwd
+#$ -V -S /bin/bash
+#$ -N hogehoge
+#$ -pe smp 24
+
+export PATH=/home/ogura/python/Python-2.7.9/bin:$PATH
+export PYTHONPATH=/home/ogura/python/Python-2.7.9/lib/python-2.7/site-packages:$PYTHONPATH
+export PYTHONHOME=/home/ogura/python/Python-2.7.9
+
+mpirun -np 8 python main.py
