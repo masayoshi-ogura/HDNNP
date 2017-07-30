3ab00a30ffdf3a6b6b4ac6049efc482d3ee07873
ogura <ogura@10.8.88.126>
2017-07-30 15:55:10 +0900

    R,angleの計算が律速だった
    get_angleではなく、直接cosine関数が用意されてるのでそちらを使う。
    少し試したところ、３倍ほど早くなるはずである。

--- my_func.py
+++ my_func.py
@@ -2,7 +2,7 @@
 
 from mpi4py import MPI
 import numpy as np
-import math
+import time
 
 # calculate the symmetry functions and derivatives of it
 ### input
@@ -14,32 +14,38 @@ import math
 # Gs: numpy array (nsample x natom x ninput)
 # dGs: numpy array (nsample x natom x 3*natom * ninput)
 def symmetric_func(comm, rank, atoms_objs, natom, nsample, ninput, Rcs, Rss, etas, lams, zetas):
+    if rank == 0:
+        st = time.time()
     Gs = np.empty((nsample, natom, ninput))
     dGs = np.empty((nsample, natom, 3*natom, ninput))
     for m in range(nsample):
-        # prepare R and angle
+        if rank == 0:
+            print 'start m:'+str(m)
+            print time.time()-st
+        # prepare R and cosine
         extend = atoms_objs[m].repeat(2)
-        pos = extend.get_positions()
         R = distance_ij(rank, extend, natom)
-        angle = angle_ijk(rank, extend, natom)
-        # prepare just slightly deviated R and angle for numerical derivatives
-        R_array,angle_array = np.empty((1+2*3*natom,8*natom)),np.empty((1+2*3*natom,8*natom,8*natom))
-        R_array[0] = R; angle_array[0] = angle
+        cosine = cosine_ijk(rank, extend, natom)
+        if rank == 0:
+            print 'checkpoint 1:'
+            print time.time()-st
+        # prepare just slightly deviated R and cosine for numerical derivatives
+        R_array,cosine_array = np.empty((1+2*3*natom,8*natom)),np.empty((1+2*3*natom,8*natom,8*natom))
+        R_array[0] = R; cosine_array[0] = cosine
         dr = 0.001
         for r in range(3*natom):
-            k,alpha = r/3,r%3
-            # copy extend and pos to prevent them from destroying
-            extend_plus = extend.copy(); newpos = pos.copy()
-            newpos[k][alpha] += dr
-            extend_plus.set_positions(newpos) # displase k-th atom along alpha-axis
+            disp_plus = np.array([dr if i==r else 0 for i in range(8*3*natom)]).reshape((8*natom,3))
+            disp_minus = np.array([-dr if i==r else 0 for i in range(8*3*natom)]).reshape((8*natom,3))
+            # prepare displaced R and cosine
+            extend_plus = extend.copy(); extend_plus.translate(disp_plus)
             R_array[2*r+1] = (distance_ij(rank, extend_plus, natom))
-            angle_array[2*r+1] = (angle_ijk(rank, extend_plus, natom))
-            # copy extend and pos to prevent them from destroying
-            extend_minus = extend.copy(); newpos = pos.copy()
-            newpos[k][alpha] -= dr
-            extend_minus.set_positions(newpos) # displase k-th atom along alpha-axis
+            cosine_array[2*r+1] = (cosine_ijk(rank, extend_plus, natom))
+            extend_minus = extend.copy(); extend_minus.translate(disp_minus)
             R_array[2*r+2] = (distance_ij(rank, extend_minus, natom))
-            angle_array[2*r+2] = (angle_ijk(rank, extend_minus, natom))
+            cosine_array[2*r+2] = (cosine_ijk(rank, extend_minus, natom))
+            if rank == 0:
+                print 'checkpoint 2, r:'+str(r)
+                print time.time()-st
         
         G = np.empty((ninput, natom))
         dG = np.empty((ninput, 3*natom, natom))
@@ -67,7 +73,7 @@ def symmetric_func(comm, rank, atoms_objs, natom, nsample, ninput, Rcs, Rss, eta
                 # G4
                 for lam in lams:
                     for zeta in zetas:
-                        G4_array = G4(comm, rank, R_array, angle_array, natom, fc_array, eta, lam, zeta)
+                        G4_array = G4(comm, rank, R_array, cosine_array, natom, fc_array, eta, lam, zeta)
                         for r in range(3*natom):
                             dG[n][r] = (G4_array[2*r+1] - G4_array[2*r+2]) / (2 * dr)
                         n += 1
@@ -98,13 +104,13 @@ def G2(comm, i, R, natom, fc, Rs, eta):
 
 # calculate symmetric function type-4
 # 3-bodies
-def G4(comm, i, R, angle, natom, fc, eta, lam, zeta):
+def G4(comm, i, R, cosine, natom, fc, eta, lam, zeta):
     G,Gi = np.empty((natom,1+2*3*natom)),np.zeros((natom,1+2*3*natom))
     R_ij = np.exp(- eta * (R**2))[:,:,None].repeat(8*natom,axis=2)
     R_ik = np.exp(- eta * (R**2))[:,None,:].repeat(8*natom,axis=1)
     fc_ij = fc[:,:,None].repeat(8*natom,axis=2)
     fc_ik = fc[:,None,:].repeat(8*natom,axis=1)
-    gi = ((1+lam * np.cos(angle*math.pi))**zeta) * R_ij * R_ik * fc_ij * fc_ik
+    gi = ((1+lam*cosine)**zeta) * R_ij * R_ik * fc_ij * fc_ik
     filter = np.array([j == i or k == i or k == j for j in range(8*natom) for k in range(8*natom)]).repeat(1+2*3*natom).reshape((1+2*3*natom,8*natom,8*natom))
     gi[filter] = 0.0
     Gi[i] = (2 ** (1-zeta)) * np.sum(gi, axis=(1,2))
@@ -113,19 +119,20 @@ def G4(comm, i, R, angle, natom, fc, eta, lam, zeta):
 
 # calculate interatomic distances
 def distance_ij(i, atoms_obj, natom):
+    # R = atoms_obj.get_all_distances(mic=True)[i]
     R = atoms_obj.get_distances(i, range(8*natom), mic=True)
     return R
 
 # calculate anlges
-def angle_ijk(i, atoms_obj, natom):
-    angle = np.zeros((8*natom,8*natom))
+def cosine_ijk(i, atoms_obj, natom):
+    cosine = np.zeros((8*natom,8*natom))
     for j in range(8*natom):
         for k in range(8*natom):
             if j == i or k == i or k == j:
                 pass
             else:
-                angle[j][k] = atoms_obj.get_angle(j,i,k)
-    return angle
+                cosine[j][k] = atoms_obj.cosine(j,i,k)
+    return cosine
 
 # calculate cutoff function
 def cutoff_func(R, Rc):
