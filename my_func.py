643188ea84142c32023edd92573443ab6b4fe1f4
ogura <ogura@10.8.88.126>
2017-07-30 14:22:04 +0900

    計算方法を少し変えてみた。処理時間変わらなければ戻す

--- my_func.py
+++ my_func.py
@@ -23,7 +23,8 @@ def symmetric_func(comm, rank, atoms_objs, natom, nsample, ninput, Rcs, Rss, eta
         R = distance_ij(rank, extend, natom)
         angle = angle_ijk(rank, extend, natom)
         # prepare just slightly deviated R and angle for numerical derivatives
-        R_plus,R_minus,angle_plus,angle_minus = [],[],[],[]
+        R_array,angle_array = np.empty((1+2*3*natom,8*natom)),np.empty((1+2*3*natom,8*natom,8*natom))
+        R_array[0] = R; angle_array[0] = angle
         dr = 0.001
         for r in range(3*natom):
             k,alpha = r/3,r%3
@@ -31,52 +32,44 @@ def symmetric_func(comm, rank, atoms_objs, natom, nsample, ninput, Rcs, Rss, eta
             extend_plus = extend.copy(); newpos = pos.copy()
             newpos[k][alpha] += dr
             extend_plus.set_positions(newpos) # displase k-th atom along alpha-axis
-            R_plus.append(distance_ij(rank, extend_plus, natom))
-            angle_plus.append(angle_ijk(rank, extend_plus, natom))
+            R_array[2*r+1] = (distance_ij(rank, extend_plus, natom))
+            angle_array[2*r+1] = (angle_ijk(rank, extend_plus, natom))
             # copy extend and pos to prevent them from destroying
             extend_minus = extend.copy(); newpos = pos.copy()
             newpos[k][alpha] -= dr
             extend_minus.set_positions(newpos) # displase k-th atom along alpha-axis
-            R_minus.append(distance_ij(rank, extend_minus, natom))
-            angle_minus.append(angle_ijk(rank, extend_minus, natom))
+            R_array[2*r+2] = (distance_ij(rank, extend_minus, natom))
+            angle_array[2*r+2] = (angle_ijk(rank, extend_minus, natom))
         
         G = np.empty((ninput, natom))
         dG = np.empty((ninput, 3*natom, natom))
         n = 0
         for Rc in Rcs:
             # prepare cutoff functions with Rc
-            fc = cutoff_func(R, Rc)
-            fc_plus,fc_minus = [],[]
-            for r in range(3*natom):
-                fc_plus.append(cutoff_func(R_plus[r], Rc))
-                fc_minus.append(cutoff_func(R_minus[r], Rc))
+            fc_array = cutoff_func(R_array, Rc)
             
             # G1
-            G[n] = G1(comm, rank, natom, fc)
+            G1_array = G1(comm, rank, natom, fc_array)
+            G[n] = G1_array[0]
             for r in range(3*natom):
-                G1_plus = G1(comm, rank, natom, fc_plus[r])
-                G1_minus = G1(comm, rank, natom, fc_minus[r])
-                dG[n][r] = (G1_plus - G1_minus) / (2 * dr)
+                dG[n][r] = (G1_array[2*r+1] - G1_array[2*r+2]) / (2 * dr)
             n += 1
             
             for eta in etas:
                 # G2
                 for Rs in Rss:
-                    G[n] = G2(comm, rank, R, natom, fc, Rs, eta)
+                    G2_array = G2(comm, rank, R_array, natom, fc_array, Rs, eta)
+                    G[n] = G2_array[0]
                     for r in range(3*natom):
-                        G2_plus = G2(comm, rank, R_plus[r], natom, fc_plus[r], Rs, eta)
-                        G2_minus = G2(comm, rank, R_minus[r], natom, fc_minus[r], Rs, eta)
-                        dG[n][r] = (G2_plus - G2_minus) / (2 * dr)
+                        dG[n][r] = (G2_array[2*r+1] - G2_array[2*r+2]) / (2 * dr)
                     n += 1
                 
                 # G4
                 for lam in lams:
                     for zeta in zetas:
-                        G[n] = G4(comm, rank, R, angle, natom, fc, eta, lam, zeta)
+                        G4_array = G4(comm, rank, R_array, angle_array, natom, fc_array, eta, lam, zeta)
                         for r in range(3*natom):
-                            G4_plus = G4(comm, rank, R_plus[r], angle_plus[r], natom, fc, eta, lam, zeta)
-                            G4_minus = G4(comm, rank, R_minus[r], angle_minus[r], natom, fc, eta, lam, zeta)
-                            dG[n][r] = (G4_plus - G4_minus) / (2 * dr)
+                            dG[n][r] = (G4_array[2*r+1] - G4_array[2*r+2]) / (2 * dr)
                         n += 1
         Gs[m] = G.T
         dGs[m] = dG.T
@@ -85,36 +78,38 @@ def symmetric_func(comm, rank, atoms_objs, natom, nsample, ninput, Rcs, Rss, eta
 # calculate symmetric function type-1
 # 2-bodies
 def G1(comm, i, natom, fc):
-    G,Gi = np.empty(natom),np.zeros(natom)
-    filter = np.array([j == i for j in range(8*natom)])
+    G,Gi = np.empty((natom,1+2*3*natom)),np.zeros((natom,1+2*3*natom))
+    filter = np.array([[j == i for j in range(8*natom)]]).repeat(1+2*3*natom, axis=0)
     fc[filter] = 0.0
-    Gi[i] = np.sum(fc)
+    Gi[i] = np.sum(fc, axis=1)
     comm.Allreduce(Gi, G, op=MPI.SUM)
-    return G
+    return G.T
 
 # calculate symmetric function type-2
 # 2-bodies
 def G2(comm, i, R, natom, fc, Rs, eta):
-    G,Gi = np.empty(natom),np.empty(natom)
+    G,Gi = np.empty((natom,1+2*3*natom)),np.zeros((natom,1+2*3*natom))
     gi = np.exp(- eta * (R - Rs) ** 2) * fc
-    filter = np.array([j == i for j in range(8*natom)])
+    filter = np.array([[j == i for j in range(8*natom)]]).repeat(1+2*3*natom, axis=0)
     gi[filter] = 0.0
-    Gi[i] = np.sum(gi)
+    Gi[i] = np.sum(gi, axis=1)
     comm.Allreduce(Gi, G, op=MPI.SUM)
-    return G
+    return G.T
 
 # calculate symmetric function type-4
 # 3-bodies
 def G4(comm, i, R, angle, natom, fc, eta, lam, zeta):
-    G,Gi = np.empty(natom),np.empty(natom)
-    R_res = np.exp(- eta * (R**2)).reshape((-1,1))
-    fc_res = fc.reshape((-1,1))
-    gi = ((1+lam * np.cos(angle*math.pi))**zeta) * np.dot(R_res, R_res.T) * np.dot(fc_res, fc_res.T)
-    filter = np.array([j == i or k == i or k == j for j in range(8*natom) for k in range(8*natom)]).reshape((8*natom,8*natom))
+    G,Gi = np.empty((natom,1+2*3*natom)),np.zeros((natom,1+2*3*natom))
+    R_ij = np.exp(- eta * (R**2))[:,:,None].repeat(8*natom,axis=2)
+    R_ik = np.exp(- eta * (R**2))[:,None,:].repeat(8*natom,axis=1)
+    fc_ij = fc[:,:,None].repeat(8*natom,axis=2)
+    fc_ik = fc[:,None,:].repeat(8*natom,axis=1)
+    gi = ((1+lam * np.cos(angle*math.pi))**zeta) * R_ij * R_ik * fc_ij * fc_ik
+    filter = np.array([j == i or k == i or k == j for j in range(8*natom) for k in range(8*natom)]).repeat(1+2*3*natom).reshape((1+2*3*natom,8*natom,8*natom))
     gi[filter] = 0.0
-    Gi[i] = (2 ** (1-zeta)) * np.sum(gi)
+    Gi[i] = (2 ** (1-zeta)) * np.sum(gi, axis=(1,2))
     comm.Allreduce(Gi, G, op=MPI.SUM)
-    return G
+    return G.T
 
 # calculate interatomic distances
 def distance_ij(i, atoms_obj, natom):
