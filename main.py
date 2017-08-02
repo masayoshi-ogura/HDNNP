f2fed1d5f913583743cb568ea985e2951811c234
Masayoshi Ogura <jas@MasayoshinoMacBook-Pro.local>
2017-08-03 00:54:40 +0900

    対称関数の計算時の並列化の仕方を変更。24ノード使えるようにした。
    また、計算済みの対称関数と未計算の対称関数を１つ１つ調べて効率よく計算するように改良

--- main.py
+++ main.py
@@ -31,41 +31,32 @@ if rank == 0:
     file = open('progress-'+datestr+'.out', 'w')
     stime = time.time()
 
-if bool.LOAD_TRAINING_DATA:
-    train_npy_dir = train_dir+'npy/'
-    Es = np.load(train_npy_dir+other.name+'-Es.npy') # nsample
-    Fs = np.load(train_npy_dir+other.name+'-Fs.npy') # nsample x 3*natom
-    Gs = np.load(train_npy_dir+other.name+'-Gs.npy') # nsample x natom x ninput
-    dGs = np.load(train_npy_dir+other.name+'-dGs.npy') # nsample x natom x 3*natom x ninput
-    hp.nsample = len(Es)
-    hp.ninput = len(Gs[0][0])
-else:
+if bool.LOAD_TRAINING_XYZ_DATA:
     train_xyz_dir = train_dir+'xyz/'
     train_npy_dir = train_dir+'npy/'
     alldataset = AtomsReader(train_xyz_dir+'AllSiGe.xyz')
-    rawdataset = [data for data in alldataset if data.config_type == 'CrystalSi0Ge8' and data.cohesive_energy < 0.0]
+    rawdataset = [data for data in alldataset if data.config_type == other.name and data.cohesive_energy < 0.0]
     cordinates = [data for data in rawdataset]
     hp.nsample = len(rawdataset)
     Es = np.array([data.cohesive_energy for data in rawdataset])
     Fs = np.array([np.array(data.force).T for data in rawdataset]).reshape((hp.nsample,3*hp.natom))
-    hp.ninput = len(hp.Rcs) + len(hp.Rcs)*len(hp.Rss)*len(hp.etas) + len(hp.Rcs)*len(hp.etas)*len(hp.lams)*len(hp.zetas)
-    Gs,dGs = my_func.symmetric_func(comm, rank, cordinates, hp.natom, hp.nsample, hp.ninput, hp.Rcs, hp.Rss, hp.etas, hp.lams, hp.zetas)
-    if rank == 0 and bool.SAVE_TRAINING_DATA:
-        train_save_dir = train_npy_dir+datestr+'/'
-        os.mkdir(train_save_dir)
-        np.save(train_save_dir+other.name+'-Es.npy', Es)
-        np.save(train_save_dir+other.name+'-Fs.npy', Fs)
-        np.save(train_save_dir+other.name+'-Gs.npy', Gs)
-        np.save(train_save_dir+other.name+'-dGs.npy', dGs)
+    hp.ninput = len(hp.Rcs) + len(hp.Rcs)*len(hp.etas)*len(hp.Rss) + len(hp.Rcs)*len(hp.etas)*len(hp.lams)*len(hp.zetas)
+    Gs,dGs = my_func.load_or_calc_G(comm, size, rank, cordinates, train_npy_dir, other.name, hp.Rcs, hp.etas, hp.Rss, hp.lams, hp.zetas, hp.natom, hp.nsample, hp.ninput)
+else:
+    train_npy_dir = train_dir+'npy/'
+    Es = np.load(train_npy_dir+other.name+'-Es.npy')
+    Fs = np.load(train_npy_dir+other.name+'-Fs.npy')
+    Gs,dGs = my_func.load_G(train_npy_dir, other.name, hp.Rcs, hp.etas, hp.Rss, hp.lams, hp.zetas) # もしなかったらつど削除する(あとでパラメータ一覧をファイルに出力するため)
+    hp.nsample = len(Es)
+    hp.ninput = len(Gs[0][0])
 dataset = [[Es[i],Fs[i],Gs[i],dGs[i]] for i in range(hp.nsample)]
 
 if rank == 0:
-    if not bool.LOAD_TRAINING_DATA:
-        file.write('Rc: '+','.join(map(str,hp.Rcs))+'\n')
-        file.write('Rs: '+','.join(map(str,hp.Rss))+'\n')
-        file.write('eta: '+','.join(map(str,hp.etas))+'\n')
-        file.write('lam: '+','.join(map(str,hp.lams))+'\n')
-        file.write('zeta: '+','.join(map(str,hp.zetas))+'\n')
+    file.write('Rc: '+','.join(map(str,hp.Rcs))+'\n')
+    file.write('eta: '+','.join(map(str,hp.etas))+'\n')
+    file.write('Rs: '+','.join(map(str,hp.Rss))+'\n')
+    file.write('lam: '+','.join(map(str,hp.lams))+'\n')
+    file.write('zeta: '+','.join(map(str,hp.zetas))+'\n')
     file.write('NN_figure: '+str(hp.ninput)+'x'+str(hp.hidden_nodes)+'x'+str(hp.hidden_nodes)+'x1\n')
     file.write('learning_rate: '+str(hp.learning_rate)+'\n')
     file.write('beta: '+str(hp.beta)+'\n')
--- my_func.py
+++ my_func.py
@@ -2,130 +2,282 @@
 
 from mpi4py import MPI
 import numpy as np
+import os.path as path
 
 # calculate the symmetry functions and derivatives of it
 ### input
-# comm,rank: MPI object
+# comm,size,rank: MPI object
 # atoms_objs: list of Atoms Object
-# Rcs,Rss,etas: list of float
+# train_npy_dir,name: string
+# Rcs,etas,Rss: list of float
+# lams,zetas: list of int
+# natoms,nsample,ninput: int
+### output
+# Gs: numpy array (nsample x natom x ninput)
+# dGs: numpy array (nsample x natom x 3*natom * ninput)
+def load_or_calc_G(comm, size, rank, atoms_objs, train_npy_dir, name, Rcs, etas, Rss, lams, zetas, natom, nsample, ninput):
+    dr = 0.01
+    quo,rem = nsample/size,nsample%size
+    if rank < rem:
+        min,max = rank*(quo+1),(rank+1)*(quo+1)
+    else:
+        min,max = rank*quo+rem,(rank+1)*quo+rem
+    Gs_T = np.empty((ninput, natom, nsample))
+    dGs_T = np.empty((ninput, 3*natom, natom, nsample))
+    n = 0
+    for Rc in Rcs:
+        # G1
+        prefix = train_npy_dir+name+'-G1-'+str(Rc)
+        if path.exists(prefix+'-Gs.npy'):
+            Gs_T[n] = np.load(prefix+'-Gs.npy').T
+            dGs_T[n] = np.load(prefix+'-dGs.npy').T
+        else:
+            G,dG = calc_G1(comm, atoms_objs, min, max, dr, Rc, natom, nsample) # calc R and fc, and then G
+            np.save(prefix+'-Gs.npy', G)
+            np.save(prefix+'-dGs.npy', dG)
+            Gs_T[n] = G.T; dGs_T[n] = dG.T
+        n += 1
+        
+        for eta in etas:
+            # G2
+            for Rs in Rss:
+                prefix = train_npy_dir+name+'-G2-'+str(Rc)+'-'+str(eta)+'-'+str(Rs)
+                if path.exists(prefix+'-Gs.npy'):
+                    Gs_T[n] = np.load(prefix+'-Gs.npy').T
+                    dGs_T[n] = np.load(prefix+'-dGs.npy').T
+                else:
+                    G,dG = calc_G2(comm, atoms_objs, min, max, dr, Rc, eta, Rs, natom, nsample) # calc R and fc, and then G
+                    np.save(prefix+'-Gs.npy', G)
+                    np.save(prefix+'-dGs.npy', dG)
+                    Gs_T[n] = G.T; dGs_T[n] = dG.T
+                n += 1
+            
+            # G4
+            for lam in lams:
+                for zeta in zetas:
+                    prefix = train_npy_dir+name+'-G4-'+str(Rc)+'-'+str(eta)+'-'+str(lam)+'-'+str(zeta)
+                    if path.exists(prefix+'-Gs.npy'):
+                        Gs_T[n] = np.load(prefix+'-Gs.npy').T
+                        dGs_T[n] = np.load(prefix+'-dGs.npy').T
+                    else:
+                        G,dG = calc_G4(comm, atoms_objs, min, max, dr, Rc, eta, lam, zeta, natom, nsample) # calc R and cosine and fc, and then G
+                        np.save(prefix+'-Gs.npy', G)
+                        np.save(prefix+'-dGs.npy', dG)
+                        Gs_T[n] = G.T; dGs_T[n] = dG.T
+                    n += 1
+    return Gs_T.T, dGs_T.T
+
+# load the symmetry functions and derivatives of it
+### input
+# train_npy_dir,name: string
+# Rcs,etas,Rss: list of float
 # lams,zetas: list of int
 ### output
 # Gs: numpy array (nsample x natom x ninput)
 # dGs: numpy array (nsample x natom x 3*natom * ninput)
-def symmetric_func(comm, rank, atoms_objs, natom, nsample, ninput, Rcs, Rss, etas, lams, zetas):
-    Gs = np.empty((nsample, natom, ninput))
-    dGs = np.empty((nsample, natom, 3*natom, ninput))
-    for m in range(nsample):
-        G = np.empty((ninput, natom))
-        dG = np.empty((ninput, 3*natom, natom))
-        n = 0
-        for Rc in Rcs:
-            # prepare R and cosine
-            atoms = atoms_objs[m]
-            atoms.set_cutoff(Rc)
-            atoms.calc_connect()
-            R = distance_ij(rank, atoms)
-            fc = cutoff_func(R, Rc)
-            cosine = cosine_ijk(rank, atoms)
-            
-            # prepare just slightly deviated R and cosine for numerical derivatives
-            # assume that neighbour atoms are not changed after displacing since dr is too small
-            R_array,fc_array,cosine_array = [],[],[]
-            R_array.append(R); fc_array.append(fc); cosine_array.append(cosine)
-            dr = 0.01
-            for r in range(3*natom):
-                k=r/3; alpha=r%3
-                # prepare displaced R and cosine
-                atoms_plus = atoms.copy(); atoms_plus.pos[k+1][alpha+1] += dr
-                atoms_plus.calc_connect()
-                R_array.append(distance_ij(rank, atoms_plus))
-                fc_array.append(cutoff_func(R_array[2*r+1], Rc))
-                cosine_array.append(cosine_ijk(rank, atoms_plus))
-                atoms_minus = atoms.copy(); atoms_minus.pos[k+1][alpha+1] -= dr
-                atoms_minus.calc_connect()
-                R_array.append(distance_ij(rank, atoms_minus))
-                fc_array.append(cutoff_func(R_array[2*r+2], Rc))
-                cosine_array.append(cosine_ijk(rank, atoms_minus))
-            
-            # G1
-            G[n] = G1(comm, rank, natom, fc_array[0])
-            for r in range(3*natom):
-                G1_plus = G1(comm, rank, natom, fc_array[2*r+1])
-                G1_minus = G1(comm, rank, natom, fc_array[2*r+2])
-                dG[n][r] = (G1_plus - G1_minus) / (2 * dr)
-            n += 1
-            
-            for eta in etas:
-                # G2
-                for Rs in Rss:
-                    G[n] = G2(comm, rank, R_array[0], natom, fc_array[0], Rs, eta)
-                    for r in range(3*natom):
-                        G2_plus = G2(comm, rank, R_array[2*r+1], natom, fc_array[2*r+1], Rs, eta)
-                        G2_minus = G2(comm, rank, R_array[2*r+2], natom, fc_array[2*r+2], Rs, eta)
-                        dG[n][r] = (G2_plus - G2_minus) / (2 * dr)
-                    n += 1
+def load_G(train_npy_dir, name, Rcs, etas, Rss, lams, zetas):
+    loaded_G,loaded_dG = [],[]
+    for Rc in Rcs:
+        prefix = train_npy_dir+name+'-G1-'+str(Rc)
+        if path.exists(prefix+'-Gs.npy'):
+            loaded_G.append(np.load(prefix+'-Gs.npy').T)
+            loaded_dG.append(np.load(prefix+'-dGs.npy').T)
+        
+        for eta in etas:
+            for Rs in Rss:
+                prefix = train_npy_dir+name+'-G2-'+str(Rc)+'-'+str(eta)+'-'+str(Rs)
+                if path.exists(prefix+'-Gs.npy'):
+                    loaded_G.append(np.load(prefix+'-Gs.npy').T)
+                    loaded_dG.append(np.load(prefix+'-dGs.npy').T)
                 
-                # G4
-                for lam in lams:
-                    for zeta in zetas:
-                        G[n] = G4(comm, rank, R_array[0], cosine_array[0], natom, fc_array[0], eta, lam, zeta)
-                        for r in range(3*natom):
-                            G4_plus = G4(comm, rank, R_array[2*r+1], cosine_array[2*r+1], natom, fc_array[2*r+1], eta, lam, zeta)
-                            G4_minus = G4(comm, rank, R_array[2*r+2], cosine_array[2*r+2], natom, fc_array[2*r+2], eta, lam, zeta)
-                            dG[n][r] = (G4_plus - G4_minus) / (2 * dr)
-                        n += 1
-        Gs[m] = G.T
-        dGs[m] = dG.T
-    return Gs, dGs
+            for lam in lams:
+                for zeta in zetas:
+                    prefix = train_npy_dir+name+'-G4-'+str(Rc)+'-'+str(eta)+'-'+str(lam)+'-'+str(zeta)
+                    if path.exists(prefix+'-Gs.npy'):
+                        loaded_G.append(np.load(prefix+'-Gs.npy').T)
+                        loaded_dG.append(np.load(prefix+'-dGs.npy').T)
+    
+    G = np.c_[loaded_G].T
+    dG = np.c_[loaded_dG].T
+    return G,dG
+
+# calculate interatomic distances, cutoff function, and cosine of triplet angle
+### input
+# atoms: Atoms Object
+# m: int
+# dr,Rc: float
+# natom: int
+### output
+# R_array,fc_array,cosine_array: list of list of numpy array
+def calc_geometry(atoms, m, dr, Rc, natom):
+    R_array,fc_array,cosine_array = [],[],[]
+    # prepare R and cosine
+    atoms.set_cutoff(Rc)
+    atoms.calc_connect()
+    R,fc = distance_ij(atoms, m, 0, Rc, natom)
+    cosine = cosine_ijk(atoms, m, 0, Rc, natom)
+    
+    # prepare just slightly deviated R and cosine for numerical derivatives
+    # assume that neighbour atoms are not changed after displacing since dr is too small
+    R_array.append(R); fc_array.append(fc); cosine_array.append(cosine),
+    dr = 0.01
+    for r in range(3*natom):
+        k=r/3; alpha=r%3
+        # prepare displaced R and cosine
+        atoms_plus = atoms.copy(); atoms_plus.pos[k+1][alpha+1] += dr
+        atoms_plus.calc_connect()
+        R_plus,fc_plus = distance_ij(atoms_plus, m, 2*r+1, Rc, natom)
+        R_array.append(R_plus); fc_array.append(fc_plus)
+        cosine_array.append(cosine_ijk(atoms_plus, m, 2*r+1, Rc, natom))
+        atoms_minus = atoms.copy(); atoms_minus.pos[k+1][alpha+1] -= dr
+        atoms_minus.calc_connect()
+        R_minus,fc_minus = distance_ij(atoms_minus, m, 2*r+2, Rc, natom)
+        R_array.append(R_minus); fc_array.append(fc_minus)
+        cosine_array.append(cosine_ijk(atoms_minus, m, 2*r+2, Rc, natom))
+        
+    return R_array,fc_array,cosine_array
+
+def calc_G1(comm, atoms_objs, min, max, dr, Rc, natom, nsample):
+    G,dG = np.empty((nsample,natom)),np.empty((nsample,natom,3*natom))
+    G_para,dG_para = np.zeros((nsample,natom)),np.zeros((nsample,natom,3*natom))
+    for m in range(min,max):
+        atoms = atoms_objs[m]
+        R,fc,cosine = calc_geometry(atoms, m, dr, Rc, natom)
+        G_para[m] = G1(fc[0], natom)
+        tmp = np.empty((3*natom,natom))
+        for r in range(3*natom):
+            G1_plus = G1(fc[2*r+1], natom)
+            G1_minus = G1(fc[2*r+2], natom)
+            tmp[r] = (G1_plus - G1_minus) / (2 * dr)
+        dG_para[m] = tmp.T
+    comm.Allreduce(G_para, G, op=MPI.SUM)
+    comm.Allreduce(dG_para, dG, op=MPI.SUM)
+    return G,dG
+
+def calc_G2(comm, atoms_objs, min, max, dr, Rc, eta, Rs, natom, nsample):
+    G,dG = np.empty((nsample,natom)),np.empty((nsample,natom,3*natom))
+    G_para,dG_para = np.zeros((nsample,natom)),np.zeros((nsample,natom,3*natom))
+    for m in range(min,max):
+        atoms = atoms_objs[m]
+        R,fc,cosine = calc_geometry(atoms, m, dr, Rc, natom)
+        G_para[m] = G2(R[0], fc[0], eta, Rs, natom)
+        tmp = np.empty((3*natom,natom))
+        for r in range(3*natom):
+            G2_plus = G2(R[2*r+1], fc[2*r+1], eta, Rs, natom)
+            G2_minus = G2(R[2*r+2], fc[2*r+2], eta, Rs, natom)
+            tmp[r] = (G2_plus - G2_minus) / (2 * dr)
+        dG_para[m] = tmp.T
+    comm.Allreduce(G_para, G, op=MPI.SUM)
+    comm.Allreduce(dG_para, dG, op=MPI.SUM)
+    return G,dG
+
+def calc_G4(comm, atoms_objs, min, max, dr, Rc, eta, lam, zeta, natom, nsample):
+    G,dG = np.empty((nsample,natom)),np.empty((nsample,natom,3*natom))
+    G_para,dG_para = np.zeros((nsample,natom)),np.zeros((nsample,natom,3*natom))
+    for m in range(min,max):
+        atoms = atoms_objs[m]
+        R,fc,cosine = calc_geometry(atoms, m, dr, Rc, natom)
+        G_para[m] = G4(R[0], fc[0], cosine[0], eta, lam, zeta, natom)
+        tmp = np.empty((3*natom,natom))
+        for r in range(3*natom):
+            G4_plus = G4(R[2*r+1], fc[2*r+1], cosine[2*r+1], eta, lam, zeta, natom)
+            G4_minus = G4(R[2*r+2], fc[2*r+2], cosine[2*r+2], eta, lam, zeta, natom)
+            tmp[r] = (G4_plus - G4_minus) / (2 * dr)
+        dG_para[m] = tmp.T
+    comm.Allreduce(G_para, G, op=MPI.SUM)
+    comm.Allreduce(dG_para, dG, op=MPI.SUM)
+    return G,dG
 
 # calculate symmetric function type-1
 # 2-bodies
-def G1(comm, i, natom, fc):
-    G,Gi = np.empty(natom),np.zeros(natom)
-    Gi[i] = np.sum(fc)
-    comm.Allreduce(Gi, G, op=MPI.SUM)
+def G1(fc, natom):
+    G = np.empty(natom)
+    for i in range(natom):
+        G[i] = np.sum(fc[i])
     return G
 
 # calculate symmetric function type-2
 # 2-bodies
-def G2(comm, i, R, natom, fc, Rs, eta):
-    G,Gi = np.empty(natom),np.zeros(natom)
-    gi = np.exp(- eta * (R - Rs) ** 2) * fc
-    Gi[i] = np.sum(gi)
-    comm.Allreduce(Gi, G, op=MPI.SUM)
+def G2(R, fc, eta, Rs, natom):
+    G = np.empty(natom)
+    for i in range(natom):
+        gi = np.exp(- eta * (R[i] - Rs) ** 2) * fc[i]
+        G[i] = np.sum(gi)
     return G
 
 # calculate symmetric function type-4
 # 3-bodies
-def G4(comm, i, R, cosine, natom, fc, eta, lam, zeta):
-    G,Gi = np.empty(natom),np.zeros(natom)
-    gauss = np.exp(- eta * (R**2)).reshape((-1,1))
-    cutoff = fc.reshape((-1,1))
-    gi = ((1+lam*cosine)**zeta) * np.dot(gauss, gauss.T) * np.dot(cutoff, cutoff.T)
-    filter = np.identity(len(R), dtype=bool)
-    gi[filter] = 0.0
-    Gi[i] = (2 ** (1-zeta)) * np.sum(gi)
-    comm.Allreduce(Gi, G, op=MPI.SUM)
+def G4(R, fc, cosine, eta, lam, zeta, natom):
+    G = np.empty(natom)
+    for i in range(natom):
+        gauss = np.exp(- eta * (R[i]**2)).reshape((-1,1))
+        cutoff = fc[i].reshape((-1,1))
+        gi = ((1+lam*cosine[i])**zeta) * np.dot(gauss, gauss.T) * np.dot(cutoff, cutoff.T)
+        filter = np.identity(len(R), dtype=bool)
+        gi[filter] = 0.0
+        G[i] = (2 ** (1-zeta)) * np.sum(gi)
     return G
 
+# memorize variables
+def memorize(f):
+    if f.func_name == 'distance_ij':
+        cache = {}
+        def helper(atoms, m, r, Rc, natom):
+            if (m,r,Rc) not in cache:
+                cache[(m,r,Rc)] = f(atoms, m, r, Rc, natom)
+            return cache[(m,r,Rc)]
+        return helper
+    elif f.func_name == 'cosine_ijk':
+        cache = {}
+        def helper(atoms, m, r, Rc, natom):
+            if (m,r,Rc) not in cache:
+                cache[(m,r,Rc)] = f(atoms, m, r, Rc, natom)
+            return cache[(m,r,Rc)]
+        return helper
+
 # calculate interatomic distances with neighbours
-def distance_ij(i, atoms):
-    R = np.array([con.distance for con in atoms.connect[i+1]])
-    return R
+### input
+# atoms: Atoms Object
+# m,r: int
+#      used for indexing cache with nsample index and displacement index
+#      m ... different for each nodes
+#      r ... 0..2*3*natom
+# Rc: float
+#     used for the same reason
+# natom: int
+### output
+# R,fc: list of numpy array
+@memorize
+def distance_ij(atoms, m, r, Rc, natom):
+    R,fc = [],[]
+    for i in range(natom):
+        R.append(np.array([con.distance for con in atoms.connect[i+1]]))
+        fc.append(np.tanh(1-R[i]/Rc)**3)
+    return R,fc
 
 # calculate anlges with neighbours
-def cosine_ijk(i, atoms):
-    n_neighb = atoms.n_neighbours(i+1)
-    cosine = np.zeros((n_neighb,n_neighb))
-    for j in range(n_neighb):
-        for k in range(n_neighb):
-            if k == j:
-                pass
-            else:
-                cosine[j][k] = atoms.cosine_neighbour(i+1,j+1,k+1)
-    return cosine
-
-# calculate cutoff function with neighbours
-def cutoff_func(R, Rc):
-    fc = np.tanh(1-R/Rc)**3
-    return fc
+### input
+# atoms: Atoms Object
+# m,r: int
+#      used for indexing cache with nsample index and displacement index
+#      m ... different for each nodes
+#      r ... 0..2*3*natom
+# Rc: float
+#     used for the same reason
+# natom: int
+### output
+# R,fc: list of numpy array
+@memorize
+def cosine_ijk(atoms, m, r, Rc, natom):
+    cosine_ret = []
+    for i in range(natom):
+        n_neighb = atoms.n_neighbours(i+1)
+        cosine = np.zeros((n_neighb,n_neighb))
+        for j in range(n_neighb):
+            for k in range(n_neighb):
+                if k == j:
+                    pass
+                else:
+                    cosine[j][k] = atoms.cosine_neighbour(i+1,j+1,k+1)
+        cosine_ret.append(cosine)
+    return cosine_ret
