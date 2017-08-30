65b0d79dc3ceee01ca555fc7bb32997676bb70c6
Masayoshi Ogura <jas@MasayoshinoMacBook-Pro.local>
2017-08-31 03:34:05 +0900

    calc_symmetric_func.pyをインポート可能に

--- calc_symmetric_func.py
+++ calc_symmetric_func.py
@@ -13,28 +13,32 @@ from quippy import AtomsReader
 # import own modules
 from modules.input import Generator
 
-# set MPI variables
-allcomm = MPI.COMM_WORLD
-allrank = allcomm.Get_rank()
-allsize = allcomm.Get_size()
 
-# set variables to all procs
-train_dir = 'training_data'
-train_xyz_file = path.join(train_dir, 'xyz', other.xyzfile)
-train_npy_dir = path.join(train_dir, 'npy', other.name)
-if not path.exists(train_npy_dir):
-    mkdir(train_npy_dir)
+def main():
+    allcomm = MPI.COMM_WORLD
+    allrank = allcomm.Get_rank()
+    allsize = allcomm.Get_size()
 
-generator = Generator(train_npy_dir, other.name, hp.Rcs, hp.etas, hp.Rss, hp.lams, hp.zetas)
+    train_dir = 'training_data'
+    train_xyz_file = path.join(train_dir, 'xyz', other.xyzfile)
+    train_npy_dir = path.join(train_dir, 'npy', other.name)
+    if not path.exists(train_npy_dir):
+        mkdir(train_npy_dir)
 
-alldataset = AtomsReader(train_xyz_file)
-coordinates = []
-for data in alldataset:
-    if data.config_type == other.name and data.cohesive_energy < 0.0:
-        coordinates.append(data)
-hp.nsample = len(coordinates)
-Es, Fs = generator.calc_EF(coordinates, hp.natom, hp.nsample)
-hp.ninput = len(hp.Rcs) + \
-    len(hp.Rcs)*len(hp.etas)*len(hp.Rss) + \
-    len(hp.Rcs)*len(hp.etas)*len(hp.lams)*len(hp.zetas)
-Gs, dGs = generator.calc_G(allcomm, allsize, allrank, coordinates, hp.natom, hp.nsample, hp.ninput)
+    generator = Generator(train_npy_dir, other.name, hp.Rcs, hp.etas, hp.Rss, hp.lams, hp.zetas)
+
+    alldataset = AtomsReader(train_xyz_file)
+    coordinates = []
+    for data in alldataset:
+        if data.config_type == other.name and data.cohesive_energy < 0.0:
+            coordinates.append(data)
+    hp.nsample = len(coordinates)
+    Es, Fs = generator.calc_EF(coordinates, hp.natom, hp.nsample)
+    hp.ninput = len(hp.Rcs) + \
+        len(hp.Rcs)*len(hp.etas)*len(hp.Rss) + \
+        len(hp.Rcs)*len(hp.etas)*len(hp.lams)*len(hp.zetas)
+    Gs, dGs = generator.calc_G(allcomm, allsize, allrank, coordinates, hp.natom, hp.nsample, hp.ninput)
+
+
+if __name__ == '__main__':
+    main()
