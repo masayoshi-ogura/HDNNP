946936b35599022307c4d5eb90c8e673f70cae54
Masayoshi Ogura <jas@MasayoshinoMacBook-Pro.local>
2017-09-11 00:29:41 +0900

    batch_sizeの増加率を設定(0でもいいかも)

--- main.py
+++ main.py
@@ -31,26 +31,29 @@ eta:  {}
 Rs:   {}
 lam:  {}
 zeta: {}
-NN_figure:     {}x{}x{}
-learning_rate: {}
-mixing_beta:   {}
-momentum:      {}
-adam_beta1:    {}
-adam_beta2:    {}
-epsilon:       {}
-nepoch:        {}
-nsample:       {}
-ninput:        {}
-batch_size:    {}
-optimizer:     {}
-activation:    {}
+NN_figure:           {}x{}x{}
+learning_rate:       {}
+learning_rate_decay: {}
+mixing_beta:         {}
+momentum:            {}
+adam_beta1:          {}
+adam_beta2:          {}
+epsilon:             {}
+nepoch:              {}
+nsample:             {}
+ninput:              {}
+batch_size:          {}
+batch_size_growth:   {}
+optimizer:           {}
+activation:          {}
 
 epoch          spent time     energy RMSE    force RMSE     RMSE
 """.format(','.join(map(str, hp.Rcs)), ','.join(map(str, hp.etas)), ','.join(map(str, hp.Rss)),
            ','.join(map(str, hp.lams)), ','.join(map(str, hp.zetas)),
            ninput, 'x'.join(map(str, hp.hidden_layer)), 1,
-           hp.learning_rate, hp.mixing_beta, hp.momentum, hp.adam_beta1, hp.adam_beta2, hp.epsilon,
-           hp.nepoch, nsample, ninput, hp.batch_size,
+           hp.learning_rate, hp.learning_rate_decay, hp.mixing_beta,
+           hp.momentum, hp.adam_beta1, hp.adam_beta2, hp.epsilon,
+           hp.nepoch, nsample, ninput, hp.batch_size, hp.batch_size_growth,
            hp.optimizer, hp.activation))
     file.flush()
 
@@ -70,7 +73,7 @@ if allrank < hp.natom:
 
     # training
     for m in range(hp.nepoch):
-        hdnnp.training(Es, Fs, Gs, dGs)
+        hdnnp.training(m, Es, Fs, Gs, dGs)
         E_RMSE, F_RMSE, RMSE = hdnnp.calc_RMSE(m, Es, Fs, Gs, dGs)
         if allrank == 0:
             file.write('{:<14} {:<14.9f} {:<14.9f} {:<14.9f} {:<14.9f}\n'.format(m+1, time()-stime, E_RMSE, F_RMSE, RMSE))
