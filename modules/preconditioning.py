b0517ffe424d49b201cf64dddd9557b7ad832245
ogura <ogura@cello.t.u-tokyo.ac.jp>
2017-10-25 01:22:17 +0900

    原子種ごとに使う主成分を変えるように変更、デフォルト閾値変更

--- modules/preconditioning.py
+++ modules/preconditioning.py
@@ -13,23 +13,33 @@ class PreconditionBase(object):
 
 
 class PCA(PreconditionBase):
-    def __init__(self, threshold=0.999):
+    def __init__(self, threshold=0.9999):
         self._threshold = threshold
 
     def decompose(self, dataset):
-        input = dataset.input
-        dinput = dataset.dinput
         if not hasattr(self, '_components'):
-            pca = decomposition.PCA()
-            pca.fit(input.reshape(-1, input.shape[-1]))
-            filter = np.add.accumulate(pca.explained_variance_ratio_) < self._threshold
-            self._mean = input.mean(axis=(0, 1))
-            self._components = pca.components_[filter].T
-            self._ncomponent = self._components.shape[1]
-            print 'decompose from {} to {}'.format(self._components.shape[0], self._components.shape[1])
+            self._mean = {}
+            self._components = {}
+            self._ncomponent = 0
+            for symbol, indices in dataset.composition['index'].items():
+                X = dataset.input[:, list(indices), :].reshape(-1, dataset.input.shape[-1])
+                pca = decomposition.PCA()
+                pca.fit(X)
+                self._mean[symbol] = X.mean(axis=0)
+                self._components[symbol] = pca.components_
+                self._ncomponent = max(self._ncomponent,
+                                       sum(np.add.accumulate(pca.explained_variance_ratio_) < self._threshold))
+            # adjust ncomponent to max of it
+            print 'decompose from {} to {}'.format(dataset.input.shape[-1], self._ncomponent)
+            for symbol, component in self._components.items():
+                self._components[symbol] = component[:self._ncomponent].T
 
-        dataset.input = np.dot(input - self._mean, self._components)
-        dataset.dinput = np.tensordot(dinput - self._mean, self._components, ((3,), (0,)))
+        mean = np.array([self._mean[symbol]
+                         for symbol in dataset.composition['symbol']])
+        components = np.array([self._components[symbol]
+                               for symbol in dataset.composition['symbol']])
+        dataset.input = np.einsum('ijk,jkl->ijl', dataset.input - mean, components)
+        dataset.dinput = np.einsum('ijkl,jlm->ijkm', dataset.dinput - mean[:, None, :], components)
         dataset.ninput = self._ncomponent
 
 
