c7f3849ebdbe920180c54f18d9941b71ec8e1c8e
ogura <ogura@cello.t.u-tokyo.ac.jp>
2017-12-16 21:02:24 +0900

    F.sqrtは関係なし。いつのまにかデータセットの方が壊れていた模様。

--- modules/model.py
+++ modules/model.py
@@ -2,7 +2,6 @@
 
 from config import hp
 
-import numpy as np
 import chainer
 import chainer.links as L
 import chainer.functions as F
@@ -10,10 +9,13 @@ from chainer import Variable
 
 
 def loss_func(y_pred, y_true, dy_pred, dy_true, obs):
-    y_loss = (1. - hp.mixing_beta) * F.mean_squared_error(y_pred, y_true)
-    dy_loss = hp.mixing_beta * F.mean_squared_error(dy_pred, dy_true)
-    loss = y_loss + dy_loss
-    chainer.report({'RMSE': np.sqrt(y_loss.data), 'd_RMSE': np.sqrt(dy_loss.data), 'tot_RMSE': np.sqrt(loss.data)}, obs)
+    y_loss = F.mean_squared_error(y_pred, y_true)
+    dy_loss = F.mean_squared_error(dy_pred, dy_true)
+    loss = (1. - hp.mixing_beta) * y_loss + hp.mixing_beta * dy_loss
+    RMSE = F.sqrt(y_loss)
+    d_RMSE = F.sqrt(dy_loss)
+    tot_RMSE = (1. - hp.mixing_beta) * RMSE + hp.mixing_beta * d_RMSE
+    chainer.report({'RMSE': RMSE, 'd_RMSE': d_RMSE, 'tot_RMSE': tot_RMSE}, obs)
     return loss
 
 
@@ -23,10 +25,10 @@ class SingleNNP(chainer.Chain):
         nodes = [None] + [h['node'] for h in hp.hidden_layers]
         self.nlink = len(hp.hidden_layers)
         with self.init_scope():
-            w = chainer.initializers.HeNormal()
+            # w = chainer.initializers.HeNormal()
             for i in range(self.nlink):
                 setattr(self, 'f{}'.format(i), eval('F.{}'.format(hp.hidden_layers[i]['activation'])))
-                setattr(self, 'l{}'.format(i), L.Linear(nodes[i], nodes[i+1], initialW=w))
+                setattr(self, 'l{}'.format(i), L.Linear(nodes[i], nodes[i+1], initialW=None))
         self.add_persistent('element', element)
 
     def __call__(self, x, dx, y_true, dy_true, train=False):
@@ -54,7 +56,6 @@ class HDNNP(chainer.ChainList):
     def __call__(self, xs, dxs, y_true, dy_true, train=False):
         xs = [Variable(x) for x in xs.transpose(1, 0, 2)]
         dxs = [Variable(dx) for dx in dxs.transpose(1, 0, 2, 3)]
-        # dxs = [[Variable(dx2) for dx2 in dx1] for dx1 in dxs.transpose(2, 1, 0, 3)]
         y_pred = self.predict_y(xs)
         dy_pred = self.predict_dy(dxs, y_pred, xs, train)
         y_pred = sum(y_pred)
@@ -66,7 +67,6 @@ class HDNNP(chainer.ChainList):
     def predict_dy(self, dxs, y, xs, train):
         dy = chainer.grad(y, xs, retain_grad=train, enable_double_backprop=train)
         return - sum([F.batch_matmul(dxi, dyi) for dxi, dyi in zip(dxs, dy)])
-        # return - F.concat([F.sum(sum([dxi * dyi for dxi, dyi in zip(dx, dy)]), axis=1, keepdims=True) for dx in dxs], axis=1)
 
     def get_by_element(self, element):
         return [nnp for nnp in self if nnp.element == element]
