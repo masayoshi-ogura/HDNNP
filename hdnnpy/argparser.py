66d98d7e5239c72fd6c28ab82ad94cd3493a1748
KeisukeYamashita <19yamashita15@gmail.com>
2018-11-22 20:04:40 +0900

    fix: args of parser function

--- hdnnpy/argparser.py
+++ hdnnpy/argparser.py
@@ -7,10 +7,13 @@ from pathlib import Path
 def get_parser():
     parser = argparse.ArgumentParser(description='High Dimensional Neural Network Potential',
                                      fromfile_prefix_chars='@')
-    parser.add_argument('--debug', '-d',
-                         action='store_true',
-                         default=False,
-                         help='enables verbose progress and debug output')
+    parser.add_argument(
+        '--debug', 
+        '-d',
+        action='store_true',
+        default=False,
+        help='enables verbose progress and debug output'
+        )
 
     subparsers = parser.add_subparsers(dest='mode')
 
@@ -23,25 +26,36 @@ def get_parser():
     phonon_parser = subparsers.add_parser('phonon', help='see `phonon -h`')
 
     # training mode
-    training_parser.add_argument('--verbose', '-v', 
-                                action='store_true', 
-                                default=False,
-                                help='this flag may increase processing time.')
-    training_parser.add_argument('--resume', '-r', 
-                                type=Path,
-                                help='resume training from given config directory.\n'
-                                      'the given directory must contain '
-                                      '`trainer_snapshot.npz`, `interim_result.pickle`.')
+    training_parser.add_argument(
+        '--verbose',
+         '-v', 
+         action='store_true', 
+         default=False,
+         help='this flag may increase processing time.'
+        )
+    training_parser.add_argument(
+        '--resume', 
+        '-r', 
+        type=Path,
+        help='resume training from given config directory.\n'
+             'the given directory must contain '
+             '`trainer_snapshot.npz`, `interim_result.pickle`.')
 
     # test mode
     for p in [prediction_parser, phonon_parser]:
-        p.add_argument('--poscar', '-p', 
-                        required=True, 
-                        type=Path,
-                        help='POSCAR file used for postprocess calculation.')
-        p.add_argument('--masters', '-m', 
-                        required=True, 
-                        type=Path,
-                        help='trained masters model used for postprocess calculation.')
+        p.add_argument(
+            '--poscar', 
+            '-p', 
+            required=True, 
+            type=Path,
+            help='POSCAR file used for postprocess calculation.'
+            )
+        p.add_argument(
+            '--masters', 
+            '-m', 
+            required=True, 
+            type=Path,
+            help='trained masters model used for postprocess calculation.'
+            )
 
     return parser.parse_args()
