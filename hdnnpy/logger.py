2c9f88e4691b21d6a6f68eb51125e7575c291b68
KeisukeYamashita <19yamashita15@gmail.com>
2018-11-22 22:17:19 +0900

    update: multi output of logger

--- hdnnpy/logger.py
+++ hdnnpy/logger.py
@@ -5,22 +5,30 @@ class Logger(object):
     def __init__(self, name="default"):
         self.logger_name = name
         self._log_level = logging.INFO
-        self._stream_log_level =  logging.INFO
         self._format = Formatter('%(levelname)s %(asctime)s %(module)s: %(message)s')
+        self._handlers = [{
+          'handler': StreamHandler(),
+          'level': logging.INFO  
+        }]
 
     def set_level(self, level):
         self._log_level = level
         self._stream_log_level = level
+        
 
     def set_format(self, format_str):
         self._format = Formatter(format_str)
+    
+    def add_handler(self, handler):
+        self._handlers.append(handler)
 
     def build(self):
         log = logging.getLogger(self.logger_name)
         log.setLevel(self._log_level)
-        stream_handler = StreamHandler()
-        stream_handler.setLevel(self._stream_log_level)
-        stream_handler.setFormatter(self._format)
-        log.addHandler(stream_handler)
-        return log
 
+        for h in self._handlers:
+            h['handler'].setLevel(h['level'])
+            h['handler'].setFormatter(self._format)
+            log.addHandler(h['handler'])
+
+        return log
