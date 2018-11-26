96c84df77f7f62c2d1b7b448354f9cf59849657a
KeisukeYamashita <19yamashita15@gmail.com>
2018-11-27 03:13:43 +0900

    update: for external logger

--- hdnnpy/hdnnp.py
+++ hdnnpy/hdnnp.py
@@ -26,8 +26,6 @@ from .util import assert_settings
 from .chainer_extensions import Evaluator
 from .chainer_extensions import set_log_scale
 from .chainer_extensions import scatter_plot
-from .logger import Logger
-
 
 def main():
     assert_settings(stg)
