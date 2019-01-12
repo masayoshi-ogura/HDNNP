8268a866fe6ee1e0ad44edf109b5569f74d0904e
ogura <ogura@cello.t.u-tokyo.ac.jp>
2019-01-13 06:27:00 +0900

    replaced symmetry function derivative calculation by chainer.grad

--- hdnnpy/dataset/atomic_structure.py
+++ hdnnpy/dataset/atomic_structure.py
@@ -5,6 +5,8 @@
 from ase.calculators.singlepoint import SinglePointCalculator
 import ase.io
 import ase.neighborlist
+import chainer
+import chainer.functions as F
 import numpy as np
 
 
@@ -16,7 +18,7 @@ class AtomicStructure(object):
           and attributes.
         | Before wrapping, it sorts atoms by element alphabetically.
         | It stores calculated neighbor information such as distance,
-          indices, and cosine.
+          indices.
 
         Args:
             atoms (~ase.Atoms): an object to wrap.
@@ -28,10 +30,10 @@ class AtomicStructure(object):
 
         results = {}
         for key, value in atoms.get_calculator().results.items():
-            if key in ['energy', 'magmom', 'free_energy']:
-                results[key] = value
+            if key in atoms.arrays:
+                results[key] = value[indices]
             else:
-                results[key] = np.array(value[indices], float)
+                results[key] = value
         calc = SinglePointCalculator(self._atoms, **results)
         self._atoms.set_calculator(calc)
 
@@ -91,15 +93,9 @@ class AtomicStructure(object):
         for key in geometry_keys:
             if (cutoff_distance not in self._cache
                     or key not in self._cache[cutoff_distance]):
-                if key in ['distance', 'distance_vector',
-                           'j2elem', 'neigh2elem', 'neigh2j']:
+                if key in ['distance_vector', 'distance', 'neigh2elem',
+                           'neigh2j']:
                     self._calculate_distance(cutoff_distance)
-                elif key in ['diff_distance']:
-                    self._calculate_diff_distance(cutoff_distance)
-                elif key in ['cosine']:
-                    self._calculate_cosine(cutoff_distance)
-                elif key in ['diff_cosine']:
-                    self._calculate_diff_cosine(cutoff_distance)
             ret.append(self._cache[cutoff_distance][key])
         for neighbor_info in zip(*ret):
             yield neighbor_info
@@ -121,44 +117,6 @@ class AtomicStructure(object):
         return [cls(atoms) for atoms
                 in ase.io.iread(str(file_path), index=':', format='xyz')]
 
-    def _calculate_cosine(self, cutoff_distance):
-        """Calculate cosine between two neighboring atoms."""
-        cosine = []
-        for dRdr, in self.get_neighbor_info(
-                cutoff_distance, ['diff_distance']):
-            cosine.append(np.dot(dRdr, dRdr.T))
-
-        self._cache[cutoff_distance].update({
-            'cosine': cosine,
-            })
-
-    def _calculate_diff_cosine(self, cutoff_distance):
-        """Calculate differentiation of cosine between two neighboring
-        atoms by atomic position."""
-        diff_cosine = []
-        for R, cos, dRdr in self.get_neighbor_info(
-                cutoff_distance, ['distance', 'cosine', 'diff_distance']):
-            dcosdr = ((dRdr[None, :, :]
-                       - dRdr[:, None, :] * cos[:, :, None])
-                      / R[:, None, None])
-            diff_cosine.append(dcosdr)
-
-        self._cache[cutoff_distance].update({
-            'diff_cosine': diff_cosine,
-            })
-
-    def _calculate_diff_distance(self, cutoff_distance):
-        """Calculate differentiation of distance to one neighboring atom
-        by atomic position."""
-        diff_distance = []
-        for r, R in self.get_neighbor_info(
-                cutoff_distance, ['distance_vector', 'distance']):
-            diff_distance.append(r / R[:, None])
-
-        self._cache[cutoff_distance].update({
-            'diff_distance': diff_distance,
-            })
-
     def _calculate_distance(self, cutoff_distance):
         """Calculate distance to one neighboring atom and store indices
         of neighboring atoms."""
@@ -166,35 +124,31 @@ class AtomicStructure(object):
         elements = sorted(set(symbols))
         index_element_map = [elements.index(element) for element in symbols]
 
-        i_list, j_list, d_list, D_list = ase.neighborlist.neighbor_list(
-            'ijdD', self._atoms, cutoff_distance)
+        i_list, j_list, D_list = ase.neighborlist.neighbor_list(
+            'ijD', self._atoms, cutoff_distance)
 
         sort_indices = np.lexsort((j_list, i_list))
         i_list = i_list[sort_indices]
         j_list = j_list[sort_indices]
-        d_list = d_list[sort_indices]
         D_list = D_list[sort_indices]
         elem_list = np.array([index_element_map[idx] for idx in j_list])
 
         i_indices = np.unique(i_list, return_index=True)[1]
         j_list = np.split(j_list, i_indices[1:])
-        distance = np.split(d_list, i_indices[1:])
-        distance_vector = np.split(D_list, i_indices[1:])
+        distance_vector = [chainer.Variable(r.astype(np.float32))
+                           for r in np.split(D_list, i_indices[1:])]
+        distance = [F.sqrt(F.sum(r**2, axis=1)) for r in distance_vector]
         elem_list = np.split(elem_list, i_indices[1:])
 
-        j2elem = []
-        neigh2elem = []
         neigh2j = []
+        neigh2elem = []
         for j, elem in zip(j_list, elem_list):
-            j2elem.append(np.unique(
-                self._atoms.get_chemical_symbols(), return_index=True)[1])
             neigh2j.append(np.searchsorted(j, range(len(symbols))))
             neigh2elem.append(np.searchsorted(elem, range(len(elements))))
 
         self._cache[cutoff_distance] = {
-            'distance': distance,
             'distance_vector': distance_vector,
-            'j2elem': j2elem,
+            'distance': distance,
             'neigh2elem': neigh2elem,
             'neigh2j': neigh2j,
             }
--- hdnnpy/dataset/descriptor/symmetry_function_dataset.py
+++ hdnnpy/dataset/descriptor/symmetry_function_dataset.py
@@ -2,10 +2,11 @@
 
 """Symmetry function dataset for descriptor of HDNNP."""
 
-from itertools import (chain, combinations_with_replacement)
+from itertools import combinations_with_replacement
 
+import chainer
+import chainer.functions as F
 import numpy as np
-from scipy.linalg import block_diag
 from tqdm import tqdm
 
 from hdnnpy.dataset.descriptor.descriptor_dataset_base import (
@@ -112,7 +113,8 @@ class SymmetryFunctionDataset(DescriptorDatasetBase):
                   for i in np.array_split(range(n_sample), MPI.size)]
         structures = self._structures[slices[MPI.rank]]
 
-        for i, send_data in enumerate(self._calculate_descriptors(structures)):
+        for i, send_data in enumerate(
+                self._calculate_descriptors(structures, verbose)):
             shape = (n_atom, self.n_feature, *(n_atom*3,) * i)
             send_data = send_data.reshape((-1,) + shape)
             if MPI.rank == 0:
@@ -127,159 +129,120 @@ class SymmetryFunctionDataset(DescriptorDatasetBase):
         if verbose:
             pprint(f'Calculated {self.name} dataset.')
 
-    def _calculate_descriptors(self, structures):
+    def _calculate_descriptors(self, structures, verbose):
         """Main method of calculating symmetry functions."""
         n_sample = len(structures)
         n_atom = len(structures[0])
 
-        functions = []
-        for function_name, params_set in self._func_param_map.items():
-            for params in params_set:
-                functions.append(eval(
-                    f'self._make_symmetry_function_{function_name}')(*params))
+        generators = []
+        for structure in structures:
+            nst = []
+            for name, params_set in self._func_param_map.items():
+                for params in params_set:
+                    nst.append(eval(
+                        f'self._symmetry_function_{name}')(structure, *params))
+            generators.append(nst)
 
         if self._order >= 0:
-            G = np.zeros((n_sample, n_atom, self.n_feature),
-                         dtype=np.float32)
-            for i, structure in enumerate(tqdm(
-                    structures, ascii=True, desc=f'Process #{MPI.rank}',
-                    leave=False, position=MPI.rank)):
-                for j, g in enumerate(zip(
-                        *[func[0](structure) for func in functions])):
-                    G[i, j] = list(chain.from_iterable(g))
-                if self._order == 0:
-                    structure.clear_cache()
-            yield G
+            if verbose:
+                pprint('Calculate symmetry function: 0th order')
+            G = []
+            with chainer.using_config('enable_backprop', self._order > 0):
+                for gen_list in tqdm(generators,
+                                     ascii=True, desc=f'Process #{MPI.rank}',
+                                     leave=False, position=MPI.rank):
+                    G.append(F.concat([next(gen) for gen in gen_list],
+                                      axis=1))
+            G = F.stack(G).reshape(
+                n_sample, n_atom, self.n_feature)
+            yield G.data
 
         if self._order >= 1:
-            dGdr = np.zeros((n_sample, n_atom, self.n_feature, n_atom, 3),
-                            dtype=np.float32)
-            for i, structure in enumerate(tqdm(
-                    structures, ascii=True, desc=f'Process #{MPI.rank}',
-                    leave=False, position=MPI.rank)):
-                for j, dgdr in enumerate(zip(
-                        *[func[1](structure) for func in functions])):
-                    dGdr[i, j] = list(chain.from_iterable(dgdr))
-                if self._order == 1:
-                    structure.clear_cache()
-            yield dGdr
+            if verbose:
+                pprint('Calculate symmetry function: 1st order')
+            dG = []
+            with chainer.using_config('enable_backprop', self._order > 1):
+                for gen_list in tqdm(generators,
+                                     ascii=True, desc=f'Process #{MPI.rank}',
+                                     leave=False, position=MPI.rank):
+                    dG.append(F.concat([next(gen) for gen in gen_list],
+                                       axis=1))
+            dG = F.stack(dG).reshape(
+                n_sample, n_atom, self.n_feature, n_atom*3)
+            yield dG.data
 
-    def _make_symmetry_function_type1(self, Rc):
-        """Define symmetry function type1 for specific parameters."""
-        def symmetry_function_type1(structure):
-            """Original symmetry function type1."""
-            for R, neigh2elem in structure.get_neighbor_info(
-                    Rc, ['distance', 'neigh2elem']):
-                g = np.tanh(1.0 - R/Rc) ** 3
-                yield [np.sum(g_e) for g_e in np.split(g, neigh2elem[1:])]
+        for structure in structures:
+            structure.clear_cache()
 
-        def diff_symmetry_function_type1(structure):
-            """1st derivative of symmetry function type1."""
-            n_atom = len(structure)
-            for R, dRdr, neigh2j, j2elem in structure.get_neighbor_info(
-                    Rc, ['distance', 'diff_distance', 'neigh2j', 'j2elem']):
-                tanh = np.tanh(1.0 - R/Rc)
-                dgdr = - 3.0 / Rc * ((1.0-tanh**2) * tanh**2)[:, None] * dRdr
-                dgdr = np.array([np.sum(dgdr_j, axis=0)
-                                 for dgdr_j in np.split(dgdr, neigh2j[1:])])
-                yield block_diag(*np.split(dgdr, j2elem[1:])).reshape(
-                    n_atom, -1, 3).transpose(1, 0, 2)
+    def _symmetry_function_type1(self, structure, Rc):
+        """Symmetry function type1 for specific parameters."""
+        G = []
+        for R, neigh2elem in structure.get_neighbor_info(
+                Rc, ['distance', 'neigh2elem']):
+            g = F.tanh(1.0 - R/Rc) ** 3
+            g = [F.sum(g_) for g_ in F.split_axis(g, neigh2elem[1:], axis=0)]
+            G.append(g)
+        yield F.stack([F.stack(a) for a in G])
 
-        if self._order == 0:
-            return symmetry_function_type1,
-        elif self._order == 1:
-            return symmetry_function_type1, diff_symmetry_function_type1
+        dG = []
+        for g, (r, neigh2j) in zip(G, structure.get_neighbor_info(
+                Rc, ['distance_vector', 'neigh2j'])):
+            dg = [[F.sum(dg_, axis=0)
+                   for dg_ in F.split_axis(chainer.grad([g_], [r])[0],
+                                           neigh2j[1:], axis=0)]
+                  for g_ in g]
+            dG.append(dg)
+        yield F.stack([F.stack([F.stack(f) for f in a]) for a in dG])
 
-    def _make_symmetry_function_type2(self, Rc, eta, Rs):
-        """Define symmetry function type2 for specific parameters."""
-        def symmetry_function_type2(structure):
-            """Original symmetry function type2."""
-            for R, neigh2elem in structure.get_neighbor_info(
-                    Rc, ['distance', 'neigh2elem']):
-                g = np.exp(-eta*(R-Rs)**2) * np.tanh(1.0 - R/Rc)**3
-                yield [np.sum(g_e) for g_e in np.split(g, neigh2elem[1:])]
+    def _symmetry_function_type2(self, structure, Rc, eta, Rs):
+        """Symmetry function type2 for specific parameters."""
+        G = []
+        for R, neigh2elem in structure.get_neighbor_info(
+                Rc, ['distance', 'neigh2elem']):
+            g = F.exp(-eta*(R-Rs)**2) * F.tanh(1.0 - R/Rc)**3
+            g = [F.sum(g_) for g_ in F.split_axis(g, neigh2elem[1:], axis=0)]
+            G.append(g)
+        yield F.stack([F.stack(a) for a in G])
 
-        def diff_symmetry_function_type2(structure):
-            """1st derivative of symmetry function type2."""
-            n_atom = len(structure)
-            for R, dRdr, neigh2j, j2elem in structure.get_neighbor_info(
-                    Rc, ['distance', 'diff_distance', 'neigh2j', 'j2elem']):
-                tanh = np.tanh(1.0 - R / Rc)
-                dgdr = (np.exp(-eta*(R-Rs)**2)
-                        * tanh**2
-                        * (-2.0*eta*(R-Rs)*tanh + 3.0/Rc*(tanh**2-1.0))
-                        )[:, None] * dRdr
-                dgdr = np.array([np.sum(dgdr_j, axis=0)
-                                 for dgdr_j in np.split(dgdr, neigh2j[1:])])
-                yield block_diag(*np.split(dgdr, j2elem[1:])).reshape(
-                    n_atom, -1, 3).transpose(1, 0, 2)
+        dG = []
+        for g, (r, neigh2j) in zip(G, structure.get_neighbor_info(
+                Rc, ['distance_vector', 'neigh2j'])):
+            dg = [[F.sum(dg_, axis=0)
+                   for dg_ in F.split_axis(chainer.grad([g_], [r])[0],
+                                           neigh2j[1:], axis=0)]
+                  for g_ in g]
+            dG.append(dg)
+        yield F.stack([F.stack([F.stack(f) for f in a]) for a in dG])
 
-        if self._order == 0:
-            return symmetry_function_type2,
-        elif self._order == 1:
-            return symmetry_function_type2, diff_symmetry_function_type2
+    def _symmetry_function_type4(self, structure, Rc, eta, lambda_, zeta):
+        """Symmetry function type4 for specific parameters."""
+        G = []
+        for r, R, neigh2elem in structure.get_neighbor_info(
+                Rc, ['distance_vector', 'distance', 'neigh2elem']):
+            cos = (r/F.expand_dims(R, axis=1)) @ (r.T/R)
+            triu = np.triu(np.ones_like(cos.data), k=1)
+            ang = F.where(triu.astype(np.bool), 1.0 + lambda_ * cos, triu)
+            g = (2.0 ** (1-zeta)
+                 * ang ** zeta
+                 * F.expand_dims(F.exp(-eta*R**2) * F.tanh(1.0 - R/Rc)**3,
+                                 axis=1)
+                 * F.expand_dims(F.exp(-eta*R**2) * F.tanh(1.0 - R/Rc)**3,
+                                 axis=0))
+            g = [F.sum(g__)
+                 for j, g_
+                 in enumerate(F.split_axis(g, neigh2elem[1:], axis=0))
+                 for k, g__
+                 in enumerate(F.split_axis(g_, neigh2elem[1:], axis=1))
+                 if j <= k]
+            G.append(g)
+        yield F.stack([F.stack(a) for a in G])
 
-    def _make_symmetry_function_type4(self, Rc, eta, lambda_, zeta):
-        """Define symmetry function type4 for specific parameters."""
-        def symmetry_function_type4(structure):
-            """Original symmetry function type4."""
-            for R, cos, neigh2elem in structure.get_neighbor_info(
-                    Rc, ['distance', 'cosine', 'neigh2elem']):
-                ang = np.triu(1.0 + lambda_*cos, k=1)
-                rad = np.exp(-eta*R**2) * np.tanh(1.0 - R/Rc)**3
-                g = 2.0**(1-zeta) * ang**zeta * rad[:, None] * rad[None, :]
-
-                yield [np.sum(g_ee)
-                       for j, g_e in enumerate(np.split(g, neigh2elem[1:]))
-                       for k, g_ee in enumerate(np.split(g_e, neigh2elem[1:],
-                                                         axis=1))
-                       if j <= k]
-
-        def diff_symmetry_function_type4(structure):
-            """1st derivative of symmetry function type4."""
-            n_elem = len(structure.elements)
-            n_comb = n_elem * (n_elem+1) // 2
-            n_atom = len(structure)
-            for R, dRdr, cos, dcosdr, neigh2elem, neigh2j, j2elem in (
-                    structure.get_neighbor_info(Rc, [
-                        'distance', 'diff_distance', 'cosine', 'diff_cosine',
-                        'neigh2elem', 'neigh2j', 'j2elem'])):
-                tanh = np.tanh(1.0 - R / Rc)
-                ang = 1.0 + lambda_*cos
-                np.fill_diagonal(ang, 0.0)
-                rad1 = np.exp(-eta*R**2) * tanh**3
-                rad2 = (np.exp(-eta*R**2)
-                        * tanh**2
-                        * (-2.0*eta*R*tanh + 3.0/Rc*(tanh**2-1.0)))
-
-                dgdr_radial_part = (2.0**(1-zeta)
-                                    * ang[:, :, None]**zeta
-                                    * rad2[:, None, None]
-                                    * rad1[None, :, None]
-                                    * dRdr[:, None, :])
-                dgdr_angular_part = (zeta * lambda_
-                                     * 2.0**(1-zeta)
-                                     * ang[:, :, None]**(zeta-1)
-                                     * rad1[:, None, None]
-                                     * rad1[None, :, None]
-                                     * dcosdr)
-                dgdr = dgdr_radial_part + dgdr_angular_part
-                dgdr = np.array(
-                    [[np.sum(dgdr_je, axis=(0, 1))
-                      for dgdr_je in np.split(dgdr_j, neigh2elem[1:], axis=1)]
-                     for dgdr_j in np.split(dgdr, neigh2j[1:])])
-
-                dGdr = np.zeros((n_comb, n_atom, 3))
-                j2elem = np.append(j2elem, 0)
-                for c, (j, k) in enumerate(combinations_with_replacement(
-                        range(n_elem), 2)):
-                    dGdr[c, j2elem[j]:j2elem[j+1]] = (
-                        dgdr[j2elem[j]:j2elem[j+1], k])
-                    dGdr[c, j2elem[k]:j2elem[k+1]] = (
-                        dgdr[j2elem[k]:j2elem[k+1], j])
-                yield dGdr
-
-        if self._order == 0:
-            return symmetry_function_type4,
-        elif self._order == 1:
-            return symmetry_function_type4, diff_symmetry_function_type4
+        dG = []
+        for g, (r, neigh2j) in zip(G, structure.get_neighbor_info(
+                Rc, ['distance_vector', 'neigh2j'])):
+            dg = [[F.sum(dg_, axis=0)
+                   for dg_ in F.split_axis(chainer.grad([g_], [r])[0],
+                                           neigh2j[1:], axis=0)]
+                  for g_ in g]
+            dG.append(dg)
+        yield F.stack([F.stack([F.stack(f) for f in a]) for a in dG])
