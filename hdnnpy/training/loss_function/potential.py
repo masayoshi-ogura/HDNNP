94c8b56d9600246f3e48c95408e9654ef34a2db1
ogura <ogura@cello.t.u-tokyo.ac.jp>
2019-01-15 17:46:39 +0900

    added new penalty term 'summation'
    renamed 'rot' keyword arugment to 'rotation'
    renamed observation key of rotation as 'RMS':'root mean square'

--- hdnnpy/training/loss_function/potential.py
+++ hdnnpy/training/loss_function/potential.py
@@ -22,7 +22,8 @@ class Potential(LossFunctionBase):
     """dict: Required orders of each dataset to calculate loss function.
     """
 
-    def __init__(self, model, properties, mixing_beta=1.0, penalty=1e-3, **_):
+    def __init__(self, model, properties, mixing_beta=1.0,
+                 summation=1e-3, rotation=1e-3, **_):
         """
         Args:
             model (HighDimensionalNNP):
@@ -34,26 +35,34 @@ class Potential(LossFunctionBase):
                 0th order property and it is equal to loss function
                 ``Zeroth``. If 1.0 it optimizes HDNNP by only 1st order
                 property.
-            penalty (float, optional):
-                Penalty term coefficient parameter. This loss function
-                adds following penalty to 1st order property vector.
+            summation (float, optional):
+                Penalty term coefficient parameter for summation of 1st
+                order property. This loss function adds following
+                 penalty to 1st order property vector.
+                :math:`\sum_{i,\alpha} F_{i,\alpha} = 0`
+            rotation (float, optional):
+                Penalty term coefficient parameter for rotation of 1st
+                order property. This loss function adds following
+                 penalty to 1st order property vector.
                 :math:`\rot \bm{F} = 0`
         """
         assert 0.0 <= mixing_beta <= 1.0
         super().__init__(model)
         self._observation_keys = [
             f'RMSE/{properties[0]}', f'RMSE/{properties[1]}',
-            f'RMSE/rot-{properties[1]}', 'RMSE/total']
+            f'M/sum-{properties[1]}', f'RMS/rot-{properties[1]}',
+            'RMSE/total']
         self._mixing_beta = mixing_beta
-        self._penalty = penalty
+        self._summation = summation
+        self._rotation = rotation
 
         if mixing_beta == 0.0:
             warnings.warn(
                 'If mixing_beta=0.0, you should use loss function type '
                 '`zeroth` instead of `first`.')
-        if penalty == 0.0:
+        if rotation == 0.0:
             warnings.warn(
-                'If penalty=0.0, you should use loss function type '
+                'If rotation=0.0, you should use loss function type '
                 '`first` instead of `first`.')
 
     def eval(self, **dataset):
@@ -77,23 +86,27 @@ class Potential(LossFunctionBase):
 
         loss0 = F.mean_squared_error(predictions[0], labels[0])
         loss1 = F.mean_squared_error(predictions[1], labels[1])
+        loss_sum = F.mean(predictions[1])
         loss_rot = F.mean_squared_error(
             predictions[2], F.swapaxes(predictions[2], 2, 3))
         total_loss = ((1.0 - self._mixing_beta) * loss0
                       + self._mixing_beta * loss1
-                      + self._penalty * loss_rot)
+                      + self._summation * loss_sum
+                      + self._rotation * loss_rot)
 
         RMSE0 = F.sqrt(loss0)
         RMSE1 = F.sqrt(loss1)
-        RMSE_rot = F.sqrt(loss_rot)
+        M_sum = loss_sum
+        RMS_rot = F.sqrt(loss_rot)
         total_RMSE = ((1.0 - self._mixing_beta) * RMSE0
                       + self._mixing_beta * RMSE1)
 
         observation = {
             self._observation_keys[0]: RMSE0,
             self._observation_keys[1]: RMSE1,
-            self._observation_keys[2]: RMSE_rot,
-            self._observation_keys[3]: total_RMSE,
+            self._observation_keys[2]: M_sum,
+            self._observation_keys[3]: RMS_rot,
+            self._observation_keys[4]: total_RMSE,
             }
         chainer.report(observation, observer=self._model)
         return total_loss
